3134de67a79cd6db7993e0dcf61d94c264776ead
IamGroooooot <dury.ko@gmail.com>
2019-12-03 19:00:25 +0900

    대여하기 버튼 눌렀을 때 정보 가져오기

--- Assets/Scripts/OnLendRequestButtonClicked.cs
+++ Assets/Scripts/OnLendRequestButtonClicked.cs
@@ -12,17 +12,21 @@ public class OnLendRequestButtonClicked : MonoBehaviour
     private void OnLendBookButtonClicked()
     {
         // 이 책의 정보를 가져온다
-
+        string bookTitle = GetBookTitle();
+        string bookAuthor = GetBookAuthor();
+        string bookPublisher = GetBookPublisher();
+        string bookLenderer = GetBookLenderer();
 
         // 책 대여하기 시도
         Debug.Log("책 대여 시도");
-
+        Debug.Log("책 정보 (제목: "+bookTitle+", 저자: "+bookAuthor+", 출판사: "+bookPublisher+", 책 주인: "+bookLenderer+")");
+        
         // 이 책 대여하기 요청
-
         // 제웅형 
+        // didLendRequestSucceed = 성공여부 
 
         // 대여하기 성공하면 대여완료 텍스트 출력
-        if(didLendRequestSucceed)
+        if (didLendRequestSucceed)
         {
             ToggleSuccessText();
         }
@@ -57,5 +61,31 @@ public class OnLendRequestButtonClicked : MonoBehaviour
         }
     }
 
-    
+    private string GetBookTitle()
+    {
+        string text = transform.parent.GetChild(0).GetComponent<UnityEngine.UI.Text>().text;
+        // "책 제목: "을 잘라서 반환
+        return text.Substring(6);
+    }
+
+    private string GetBookAuthor()
+    {
+        string text = transform.parent.GetChild(1).GetComponent<UnityEngine.UI.Text>().text;
+        // "저자: "을 잘라서 반환
+        return text.Substring(4);
+    }
+
+    private string GetBookPublisher()
+    {
+        string text = transform.parent.GetChild(2).GetComponent<UnityEngine.UI.Text>().text;
+        // "출판사: "을 잘라서 반환
+        return text.Substring(5);
+    }
+
+    private string GetBookLenderer()
+    {
+        string text = transform.parent.GetChild(3).GetComponent<UnityEngine.UI.Text>().text;
+        // "책 주인: "을 잘라서 반환
+        return text.Substring(6);
+    }
 }
