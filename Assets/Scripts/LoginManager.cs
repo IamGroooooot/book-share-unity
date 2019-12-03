0bb2d5ec75dde1b194ee9b7244a61ffbf0300828
IamGroooooot <dury.ko@gmail.com>
2019-12-03 19:37:10 +0900

    로그인관련 데이터 가져오기 함수 만들었음

--- Assets/Scripts/LoginManager.cs
+++ Assets/Scripts/LoginManager.cs
@@ -15,6 +15,17 @@ public class LoginManager : MonoBehaviour
 
     public void OnLoginButtonClicked()
     {
+        // get data
+        string id = GetUserID();
+        string password = GetUserPassword();
+
+        Debug.Log("로그인 시도");
+        Debug.Log("로그인 정보(ID: "+ id + ",Password: " + password + ")");
+       
+        // 로그인 시도
+        // 제웅형
+        // isValid = 로그인성공여부
+
         if (isValid)
         {
             Debug.Log("로그인 성공");
@@ -29,6 +40,7 @@ public class LoginManager : MonoBehaviour
 
     }
 
+    // 회원가입 버튼 눌렀을 때
     public void OnSignupButtonClicked()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(1);
@@ -38,5 +50,17 @@ public class LoginManager : MonoBehaviour
     public void OnAlertPanelClicked()
     {
         alertPanel.SetActive(false);
+    }
+
+    // 유저가 입력한 ID 가져옴
+    private string GetUserID()
+    {
+        return GameObject.Find("ID_InputField").GetComponent<UnityEngine.UI.InputField>().text;
+    }
+
+    // 유저가 입력한 Password 가져옴
+    private string GetUserPassword()
+    {
+        return GameObject.Find("PW_InputField").GetComponent<UnityEngine.UI.InputField>().text;
     }
 }
